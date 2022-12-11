62a5fb2f93141dbb36746e1c5b5b73536db03819
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2022-12-10 16:35:29 -0800

    debug color changing functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var today = dayjs();
+var currentHour = dayjs().hour()
 var saveButtonEl = $(".saveBtn");
 var hour9 = $("#hour-9");
 var hour10 = $("#hour-10");
@@ -10,27 +11,11 @@ var hour15 = $("#hour-15");
 var hour16 = $("#hour-16");
 var hour17 = $("#hour-17");
 
+console.log(today)
 
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-  
-  saveButtonEl.on("click", function () {
 
-    // console.log("9am: ", hour9Events.children("textarea").val());
-    // console.log("10am: ", hour10Events.children("textarea").val());
-    // console.log("11am: ", hour11Events.children("textarea").val());
+  saveButtonEl.on("click", function () {
 
     localStorage.setItem("9am: ", hour9.children("textarea").val());
     localStorage.setItem("10am: ", hour10.children("textarea").val());
@@ -43,37 +28,112 @@ $(function () {
     localStorage.setItem("5pm: ", hour17.children("textarea").val());
   })
   
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  // for hour 9, if today = 9am, then remove past and future class, else if today is before 9am, then remove past and present class, else if today is after 9am, then remove present and future class
-
-
-  if (today > 9) {
-    hour9.removeClass("present")
-    hour9.removeClass("future")
-  } else if (today < 9) {
+  if (currentHour === 9) {
     hour9.removeClass("past")
+    hour9.removeClass("future") 
+  } else if (currentHour < 9) {
+    hour9.removeClass("future")
     hour9.removeClass("present")
   } else {
+    hour9.removeClass("present")
     hour9.removeClass("past")
-    hour9.removeClass("future")
   }
 
-  
-  
+  if (currentHour === 10) {
+    hour10.removeClass("past")
+    hour10.removeClass("future") 
+  } else if (currentHour < 10) {
+    hour10.removeClass("future")
+    hour10.removeClass("present")
+  } else {
+    hour10.removeClass("present")
+    hour10.removeClass("past")
+  }
 
+  if (currentHour === 11) {
+    hour11.removeClass("past")
+    hour11.removeClass("future") 
+  } else if (currentHour < 11) {
+    hour11.removeClass("future")
+    hour11.removeClass("present")
+  } else {
+    hour11.removeClass("present")
+    hour11.removeClass("past")
+  }
+
+  if (currentHour === 12) {
+    hour12.removeClass("past")
+    hour12.removeClass("future") 
+  } else if (currentHour < 12) {
+    hour12.removeClass("future")
+    hour12.removeClass("present")
+  } else {
+    hour12.removeClass("present")
+    hour12.removeClass("past")
+  }
+
+  if (currentHour === 13) {
+    hour13.removeClass("past")
+    hour13.removeClass("future") 
+  } else if (currentHour < 13) {
+    hour13.removeClass("future")
+    hour13.removeClass("present")
+  } else {
+    hour13.removeClass("present")
+    hour13.removeClass("past")
+  }
+
+  if (currentHour === 14) {
+    hour14.removeClass("past")
+    hour14.removeClass("future") 
+  } else if (currentHour < 14) {
+    hour14.removeClass("future")
+    hour14.removeClass("present")
+  } else {
+    hour14.removeClass("present")
+    hour14.removeClass("past")
+  }
+
+  if (currentHour === 15) {
+    hour15.removeClass("past")
+    hour15.removeClass("future") 
+  } else if (currentHour < 15) {
+    hour15.removeClass("future")
+    hour15.removeClass("present")
+  } else {
+    hour15.removeClass("present")
+    hour15.removeClass("past")
+  }
+
+  if (currentHour === 16) {
+    hour16.removeClass("past")
+    hour16.removeClass("future") 
+  } else if (currentHour < 16) {
+    hour16.removeClass("future")
+    hour16.removeClass("present")
+  } else {
+    hour16.removeClass("present")
+    hour16.removeClass("past")
+  }
+
+  if (currentHour === 17) {
+    hour17.removeClass("past")
+    hour17.removeClass("future") 
+  } else if (currentHour < 17) {
+    hour17.removeClass("future")
+    hour17.removeClass("present")
+  } else {
+    hour17.removeClass("present")
+    hour17.removeClass("past")
+  }
 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  
+  var hour9Storage = localStorage.getItem("9am: ");
+  hour9.children("textarea").append(hour9Storage);
 
   //
   // TODO: Add code to display the current date in the header of the page.
